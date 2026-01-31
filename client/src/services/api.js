3bab9a7961f8f516708cddd69da5d8a785f50194
nirmal-2512 <nirmalpatidar2533@gmail.com>
2026-01-31 17:42:47 +0530

    Fix SPA routing on Vercel

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskmangerformgt.vercel.app/",
+  baseURL: "https://taskmangerformgt.vercel.app",
 });
 
 api.interceptors.request.use((req) => {
