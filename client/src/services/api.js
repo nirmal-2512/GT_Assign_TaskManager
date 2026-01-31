99b64e90f509fadf11ac3103943cf0ae8ae60dca
nirmal-2512 <nirmalpatidar2533@gmail.com>
2026-01-31 16:30:50 +0530

    Prepare for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:4000/api",
+  baseURL: "https://gtassigntaskmanager1.vercel.app/api",
 });
 
 api.interceptors.request.use((req) => {
