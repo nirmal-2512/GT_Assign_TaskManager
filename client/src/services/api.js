39a3775ee2018dc100b7f9c1a9ce962f07d3e40d
nirmal-2512 <nirmalpatidar2533@gmail.com>
2026-01-31 18:31:28 +0530

    Fix SPA routing on Vercel New URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
  
-  baseURL: "https://taskmanagerforgt.onrender.com/api",
+  baseURL: "https://gt-assign-taskmanager.onrender.com/api",
 });
 
 api.interceptors.request.use((req) => {
